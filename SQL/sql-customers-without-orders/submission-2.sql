9d264b79c37b8bc09559b8c6b74919d648fc7dde
Brandon Luu <103155874+brandoluu@users.noreply.github.com>
2026-05-22 12:47:58 -0700

    Add: sql-customers-without-orders - submission-2

---
+++ SQL/sql-customers-without-orders/submission-2.sql
@@ -0,0 +1,20 @@
+-- Write your query below
+
+/*
+customers:
+id - int
+name varchar
+
+orders:
+id - int
+customer_id - customers(id)
+
+Goal: write query to find all customers who have never placed an order
+*/
+
+SELECT name 
+FROM customers
+WHERE id NOT IN (
+    SELECT customer_id 
+    FROM orders
+);
